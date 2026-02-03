371bd3a6e2c13e161eb8e65fce4abc3b0e67f5ff
Pawel Toloczmanski <biuro@techiot.pl>
2026-02-03 09:23:58 +0100

    Update Szambo Control PWA

--- szambo_control/flutter_service_worker.js
+++ szambo_control/flutter_service_worker.js
@@ -12,8 +12,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "083e4afe8c6b47e6277af18d68efffbf
 "assets/assets/tank.png": "97cbac28150a6a62809f0ac01400a01b",
 "assets/assets/techiot.png": "6fc8ae40c93b72101420fdfa23be8364",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
-"assets/fonts/MaterialIcons-Regular.otf": "8042d5d599e13e88cc97e4c886a7485a",
-"assets/NOTICES": "0e90d6a02454d46f05644c8085d1b1fc",
+"assets/fonts/MaterialIcons-Regular.otf": "9ac8cd54885e52a5b0665d1cf6f1f096",
+"assets/NOTICES": "2f5cb976f0bfba7bc108b6d7829e9729",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "canvaskit/canvaskit.js": "728b2d477d9b8c14593d4f9b82b484f3",
 "canvaskit/canvaskit.js.symbols": "bdcd3835edf8586b6d6edfce8749fb77",
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "083e4afe8c6b47e6277af18d68efffbf
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "63c6cae1b27e241f7d0e8d08f70e46d4",
+"flutter_bootstrap.js": "5dfcb774eb9ba37c182a0944075739d9",
 "icons/apple-touch-icon-180x180.png": "1f7329c18f18facf9d510f972f19bb4c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -34,9 +34,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "083e4afe8c6b47e6277af18d68efffbf
 "icons/Icon-maskable-512.png": "8045ef6b68a5da92c9043452149fee27",
 "index.html": "56674849a4e7d29d6913e1114824e1b4",
 "/": "56674849a4e7d29d6913e1114824e1b4",
-"main.dart.js": "5c9831369eefe4aaa13ce26bbd15f1fd",
+"main.dart.js": "3b9e0a693879b24b3cbf7b56e77a6af7",
 "manifest.json": "f718a24fd24899f65faea8eb1b2257ff",
-"version.json": "2d97ae24b5825438dcdb1cb334b42d26"};
+"version.json": "a7fd3645a9983d318aac20f71bba8d06"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
